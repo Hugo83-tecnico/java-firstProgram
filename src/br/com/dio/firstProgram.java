a12bbd35a8ac6e52a1d5c68260da0c36adb7472e
Hugo83-tecnico <hugo83_tecnico@hotmail.com>
2023-06-07 11:14:22 -0300

    first commit

---
+++ src/br/com/dio/firstProgram.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class firstProgram {
+    public static void main(String[] args) {
+        Animal gato = new Animal("Meg","Azul",7);
+        System.out.println(gato);
+
+    }
+}
